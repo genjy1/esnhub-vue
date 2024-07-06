e1e44d235a779b49e8622643f85d25c1dfa06c2c
genjy1 <idontknower1997@gmail.com>
2024-07-07 01:36:22 +0300

    feat:added gif for 404 view

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1 class="text-3xl font-display font-extrabold text-center pt-96 text-purple-700">
+  <h1 class="text-3xl font-display font-extrabold text-center pt-48 text-purple-700">
     Unluko! This page does not exist!
   </h1>
+  <div class="w-4/5 mx-auto my-0">
+    <img src="../assets/unluko.gif" alt="unluko gif" class="mx-auto my- mt-12" />
+  </div>
 </template>
